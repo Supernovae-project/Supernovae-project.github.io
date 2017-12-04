82b2dc3dc4a26f2d6e3c7e217b2a72f8b8175764
Kyle Anderson <KyleAnderson@Kyles-MacBook-Pro-9.local>
2017-12-04 04:35:54 -0500

    Commit all files

---
+++ sourcing.js
@@ -0,0 +1,37 @@
+function source(val){
+  if (val == 'bennet'){
+    alert("Bennet, Jeffrey, et al. “Chapter 13: Star Stuff.” The Essential Cosmic Perspective, 7th ed., Pearson, 2015, session.masteringastronomy.com/myct/mastering#/.");
+  } else if (val == 'britannica'){
+    alert("The Editors of Encyclopædia Britannica. “Supernova.” Encyclopædia Britannica, Encyclopædia Britannica, Inc., 6 Oct. 2016, www.britannica.com/topic/supernova.");
+  } else if (val == 'billings') {
+    alert("Billings, Lee. “Found: The Most Powerful Supernova Ever Seen.” Scientific American, 14 Jan. 2016, www.scientificamerican.com/article/found-the-most-powerful-supernova-ever-seen/.");
+  } else if (val == 'kaplan') {
+    alert("Kaplan, Sarah. “'The Strangest Supernova We’Ve Ever Seen': A Star That Keeps Exploding - and Surviving.” The Washington Post, WP Company, 8 Nov. 2017, www.washingtonpost.com/news/speaking-of-science/wp/2017/11/08/the-strangest-supernova-weve-ever-seen-a-star-that-keeps-exploding-and-surviving/?utm_term=.490a88233787.");
+  } else if (val == 'mack-blackHole') {
+    alert("Mack, Eric. “Black Hole Rips Star Apart, Creating Biggest Blast Ever Seen.” CNET, 12 Dec. 2016, www.cnet.com/news/black-hole-rips-star-apart-biggest-blast-ever-seen/.")
+  } else if (val == 'mack-supernova') {
+    alert("Mack, Eric. “Supernova Spotted Just Hours after Star's Death.” CNET, 13 Feb. 2017, www.cnet.com/news/supernova-iptf13dqy-spotted-hours-after-star-explosion-nature-physics/.")
+  } else if (val == 'may') {
+    alert("May, Sandra. “What Is a Supernova?” NASA, NASA, 1 June 2015, www.nasa.gov/audience/forstudents/5-8/features/nasa-knows/what-is-a-supernova.html.");
+  } else if (val == 'nave') {
+    alert("Nave, Rod. “Supernovae.” Hyperphysics, Georgia State University, hyperphysics.phy-astr.gsu.edu/hbase/Astro/snovcn.html.");
+  } else if (val == 'parks') {
+    alert("Parks, Jake. “This Star Went Supernova ... And Then Went Supernova Again.” Discover Magazine, 9 Nov. 2017, blogs.discovermagazine.com/d-brief/2017/11/09/zombie-star-went-supernova-twice/#.WiMeC7Q-egA.");
+  } else if (val == 'tasoff') {
+    alert("Tasoff, Harrison. “Bizarre 3-Year-Long Supernova Defies Our Understanding of How Stars Die.” Space.com, 8 Nov. 2017, www.space.com/38708-bizarre-supernova-defies-understanding.html.");
+  } else if (val == 'wolchover') {
+    alert("Wolchover, Natalie. “'Crazy' Supernova Looks Like a New Kind of Star Death.” Quanta Magazine, 8 Nov. 2017, www.quantamagazine.org/crazy-supernova-looks-like-a-new-kind-of-star-death-20171108/.");
+  } else if (val == 'cooper') {
+    alert("Cooper, Charles. “Kepler’s Famous Supernova Revealed (Pictures).” CNET,www.cnet.com/pictures/keplers-famous-supernova-revealed-pictures.");
+  } else if (val == 'siegal') {
+    alert("Siegal, Ethan. “What Will Happen When Betelgeuse Explodes?” Forbes, Forbes Magazine, 27 Mar. 2017, https://www.forbes.com/sites/startswithabang/2017/03/22/what-will-happen-when-betelgeuse-explodes/#11ffe3dd13ac.");
+  } else if (val =="massey"){
+    alert("Massey, Robert. “Did Exploding Stars Help Life on Earth Thrive?” Royal Astronomical Society, 24 Apr. 2012, www.ras.org.uk/news-and-press/219-news-2012/2117-did-exploding-stars-help-life-on-earth-to-thrive.");
+  } else if (val == "choi") {
+    alert("Choi, Charles Q. “Exploding Star May Have Sparked Formation of Our Solar System.”Space.com, 6 Aug. 2012, www.space.com/16943-supernova-explosion-solar-system-formation.html.");
+  } else if (val == "vid"){
+    alert("CyberWebFX, director. DeepSpace Nebula - Free HD Stock-Footage Motion Graphic.Youtube, Www.youtube.com, 19 July 2015, www.youtube.com/watch?v=EaM9kDPa_AY.");
+  }else {
+    alert("See Work Cited For Full List of Sources")
+  }
+}
